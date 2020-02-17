844c8ae64c92321dc938a0041053fce82c551ec0
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-17 21:21:11 +0900

    x

--- src/step1/Bj_10718.java
+++
@@ -1,9 +0,0 @@
-package step1;
-
-public class Bj_10718 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("강한친구 대한육군");
-		System.out.println("강한친구 대한육군");
-	}
-}
--- src/step1/Bj_2557.java
+++
@@ -1,9 +0,0 @@
-package step1;
-public class Bj_2557 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
-
-}
