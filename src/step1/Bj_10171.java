646f9945863bdbeb44f0d124c02bcfe5e74c91ba
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-17 21:21:40 +0900

    step1 완료

---
+++ src/step1/Bj_10171.java
@@ -0,0 +1,12 @@
+package step1;
+public class Bj_10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+	    System.out.println("(  /  )");
+		System.out.print(" \\(__)|");
+	}
+
+}
