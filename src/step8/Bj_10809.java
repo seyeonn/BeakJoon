d5b1cb19e62d91980fa1a7648dd543f4818de7af
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-20 20:17:58 +0900

    0220 도전중

---
+++ src/step8/Bj_10809.java
@@ -0,0 +1,22 @@
+package step8;
+
+import java.util.Scanner;
+
+public class Bj_10809 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		String s = scan.next();
+		
+		for(int i=0; i<25; i++) {
+			
+		}
+
+
+
+
+	}
+
+}
---
+++ src/step8/Bj_2675.java
@@ -0,0 +1,25 @@
+package step8;
+
+import java.util.Scanner;
+
+public class Bj_2675 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int t = scan.nextInt();
+		for(int i=0; i<t; i++) {
+			int r = scan.nextInt();	
+			String s = scan.next();
+			
+			for(int j=0; j<s.length(); j++) {
+				for(int k=0; k<r; k++) {
+					System.out.print(s.charAt(j));
+				}
+			}
+		}
+
+	}
+
+}
