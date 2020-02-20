65bf03108e54e025f8bfba18862c01f091de145c
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-20 21:23:39 +0900

    0220 해결중

---
+++ src/step8/Bj_2941.java
@@ -0,0 +1,29 @@
+package step8;
+
+import java.util.Scanner;
+
+public class Bj_2941 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		String words = scan.next();
+
+		char[] arr = words.toCharArray();
+
+		int sum =0;
+
+		for(int i=0; i<arr.length; i++) {
+			cro(arr[i]);	
+		}
+	}
+
+	static int cro(char c) {
+		switch (c) {
+		case 'c=':
+		case 'c-':
+			
+		}
+	}
+}
