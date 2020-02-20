e714634341359c2640fa0d100a47542c49665950
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-20 20:29:34 +0900

    0220 도전중

---
+++ src/step8/Bj_1157.java
@@ -0,0 +1,18 @@
+package step8;
+
+import java.util.Scanner;
+
+public class Bj_1157 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		String c = scan.next();
+
+		for(int i=0; i<c.length(); i++) {
+
+		}
+	}
+
+}
