23cc5735489e5700c6b2e0700740e4d218bd8d43
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-20 20:00:17 +0900

    0220 도전중

--- src/step8/Bj_11720.java
+++ src/step8/Bj_11720.java
@@ -1,10 +1,22 @@
 package step8;
 
+import java.util.Scanner;
+
 public class Bj_11720 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
 
+		int n = scan.nextInt();
+		int total =0;
+		
+		for(int i=0; i<n; i++) {
+			char c = scan.next().charAt(i);
+			total += c;
+		}
+		
+		System.out.println(total);
 	}
 
 }
