d383690bbfd68ff0e2db63500448d9b87ec509be
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-20 21:50:26 +0900

    0220 해결중

---
+++ src/step8/Bj_1316.java
@@ -0,0 +1,20 @@
+package step8;
+
+import java.util.Scanner;
+
+public class Bj_1316 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+
+		for(int i=0; i<n; i++) {
+			String s = scan.next();
+		}
+		
+		
+	}
+
+}
