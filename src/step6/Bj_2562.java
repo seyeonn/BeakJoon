4633e67fd49a7bc3ea6888756d3897e913bc69d7
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-21 15:26:37 +0900

    0221 해결

--- src/step6/Bj_2562.java
+++ src/step6/Bj_2562.java
@@ -14,9 +14,9 @@ public class Bj_2562 {
 			arr[i] = scan.nextInt();
 		}
 		
-		int max = arr[0];
-		int index = 0;
-		for(int i=1; i<arr.length; i++) {
+		int max = 0;
+		int index = 1;
+		for(int i=0; i<arr.length; i++) {
 			if(arr[i] > max) {
 				max = arr[i];
 				index = i+1;
