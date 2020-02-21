5294e47c43e908255f4f120375af2466de82982d
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-21 17:36:31 +0900

    0221 드디어 해결

--- src/step6/Bj_3052.java
+++ src/step6/Bj_3052.java
@@ -1,5 +1,6 @@
 package step6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bj_3052 {
@@ -13,14 +14,23 @@ public class Bj_3052 {
 		for(int i=0; i<arr.length; i++) {
 			arr[i] = scan.nextInt();
 		}
-		
+
 		int[] mod = new int[10];
 		for(int i=0; i<arr.length; i++) {
 			mod[i] = arr[i]%42;
 		}
-		
-		int count =0;
-		
+
+		Arrays.sort(mod);
+
+		int count = 1;
+		for(int i=0; i<mod.length-1; i++) {
+				if(mod[i] != mod[i+1])
+					count++;
+			}
+
+		System.out.println(count);
+
+
 	}
 
 }
