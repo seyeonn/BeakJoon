77482b2f1a153bfabe21aab3aaf0fbf2585d26d2
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-21 16:16:29 +0900

    0221 해결

---
+++ src/step3/Bj_15552.java
@@ -0,0 +1,28 @@
+package step3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Bj_15552 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.write(a+b+"\n");
+		}
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
