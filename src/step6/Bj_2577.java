e2f1bd300aaec53be8f532043d67ddb1a6ecd780
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-21 16:39:59 +0900

    0221 해결

--- src/step6/Bj_2577.java
+++ src/step6/Bj_2577.java
@@ -12,10 +12,17 @@ public class Bj_2577 {
 		int b = scan.nextInt();
 		int c = scan.nextInt();
 
-		int total = a*b*c;
+		String str = Integer.toString(a*b*c);
 		
+		int[] arr = new int[10];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = 0;
+			for(int j=0; j<str.length(); j++)
+				if(i == str.charAt(j)-'0')
+					arr[i]++;
+			System.out.println(arr[i]);
+		}
 		
-
 	}
 
 }
