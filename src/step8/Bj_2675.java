d365cd8bc150928cd790446e0b5e23b05a056a75
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-22 21:03:15 +0900

    0222 해결

--- src/step8/Bj_2675.java
+++ src/step8/Bj_2675.java
@@ -9,18 +9,21 @@ public class Bj_2675 {
 		Scanner scan = new Scanner(System.in);
 
 		int t = scan.nextInt();
+
+		String temp = "";
+		
 		for(int i=0; i<t; i++) {
 			int r = scan.nextInt();	
 			String s = scan.next();
-			
+
 			for(int j=0; j<s.length(); j++) {
 				for(int k=0; k<r; k++) {
-					System.out.print(s.charAt(j));
+					temp += s.charAt(j);
 				}
-				System.out.println();
 			}
+			temp += "\n";
 		}
-
+		System.out.println(temp);
 	}
 
 }
