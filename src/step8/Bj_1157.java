d8f24b5bcdf3232549895b4a903280094854b369
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-23 23:12:13 +0900

    0223 해결

--- src/step8/Bj_1157.java
+++ src/step8/Bj_1157.java
@@ -8,11 +8,26 @@ public class Bj_1157 {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 
-		String c = scan.next();
-
+		String c = scan.next().toUpperCase();
+		
+		int[] arr = new int[26];
+		
+		int max =0;
+		char result = '?';
+		
 		for(int i=0; i<c.length(); i++) {
-
+			int al = c.charAt(i)-65;
+			arr[al]++;
+			
+			if(max<arr[al]) {
+				max = arr[al];
+				result = c.charAt(i);
+			}
+			else if(max == arr[al]) {
+				result = '?';
+			}
 		}
+		System.out.println(result);
 	}
 
 }
