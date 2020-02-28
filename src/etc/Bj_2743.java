2120b4e22f1e813fd233f8abfdd87cf60e50c850
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-29 02:27:50 +0900

    0229

---
+++ src/etc/Bj_2743.java
@@ -0,0 +1,16 @@
+package etc;
+
+import java.util.Scanner;
+
+public class Bj_2743 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		String a = scan.next();
+		
+		System.out.println(a.length());
+	}
+
+}
