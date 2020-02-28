59409f9497341c02b525af3190d66b8c05a379e3
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-02-29 02:15:02 +0900

    0229 해결

--- src/step7/Bj_1065.java
+++ src/step7/Bj_1065.java
@@ -9,8 +9,27 @@ public class Bj_1065 {
 		Scanner scan = new Scanner(System.in);
 
 		int n = scan.nextInt();
+		int count = 0;
 
+		for(int i=1; i<=n; i++) {
+			if(check(i)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
 
+	static boolean check(int n) {
+		boolean result = false;
+		if(n<100)
+			result = true;
+		else if(n>=100 && n<1000) {
+			int f = (n/100)-(n%100)/10;
+			int s = (n%100)/10 - n%10;
+			if(f == s)
+				result = true;
+		}
+		return result;
 	}
 
 }
