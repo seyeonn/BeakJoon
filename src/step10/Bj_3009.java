d56fc7c7871b21c42e27afb069aed17d98a82f56
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-03-04 23:49:59 +0900

    0304 해결

--- src/step10/Bj_3009.java
+++ src/step10/Bj_3009.java
@@ -7,25 +7,38 @@ public class Bj_3009 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		int arr[] = new int[6];
 
-		for(int i=0; i<arr.length; i++)
-			arr[i] = scan.nextInt();
+		int x[] = new int[3];
+		int y[] = new int[3];
 
-		for(int i=0; i<arr.length; i++) {
-			for(int j=i+1; j<arr.length; j++) {
-				if(arr[i] ==arr[j]) {
-					arr[i] = 0;
-					arr[j] = 0;
+		for(int i=0; i<3; i++) {
+			x[i] = scan.nextInt();
+			y[i] = scan.nextInt();
+		}
+
+		for(int i=0; i<2; i++) {
+			for(int j=i+1; j<3; j++) {
+				if(x[i] ==x[j]) {
+					x[i] = 0;
+					x[j] = 0;
+				}
+				if(y[i] == y[j]) {
+					y[i]=0;
+					y[j]=0;
 				}
 			}
 		}
-		
-		for(int k=0; k<arr.length; k++) {
-			if(arr[k] != 0)
-				System.out.print(arr[k]+" ");
+		int x1 = 0;
+		int y1 = 0;
+
+		for(int k=0; k<3; k++) {
+			if(x[k] != 0)
+				x1 = x[k];
+			if(y[k] != 0)
+				y1 = y[k];
 		}
+		System.out.println(x1+ " " + y1);
 	}
-	
+
 
 }
