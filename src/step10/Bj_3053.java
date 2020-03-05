383eb8f665f3317e61567a2a5cf1ca506fa13312
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-03-05 17:07:33 +0900

    0305

---
+++ src/step10/Bj_3053.java
@@ -0,0 +1,17 @@
+package step10;
+
+import java.util.Scanner;
+
+public class Bj_3053 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int r = scan.nextInt();
+
+		System.out.println(String.format("%.6f",(double)(r*r*Math.PI)));
+		System.out.println(String.format("%.6f",(double)(r*r*2)));
+	}
+
+}
