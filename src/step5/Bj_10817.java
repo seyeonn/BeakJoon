48c90a9d6ad15ce89bda07c47274fb4ebe7165ad
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-03-14 21:22:06 +0900

    step5 이동

---
+++ src/step5/Bj_10817.java
@@ -0,0 +1,23 @@
+package step5;
+
+import java.util.Scanner;
+
+public class Bj_10817 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+
+		if(b<=a&&a<=c || c<=a&&a<=b)
+			System.out.println(a);
+		else if(a<=b&&b<=c || c<=b&&b<=a)
+			System.out.println(b);
+		else
+			System.out.println(c);
+	}
+
+}
