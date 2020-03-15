898e17a89e84c164043468b76b5bd4d0310c9cd1
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-03-15 22:33:24 +0900

    0315 해결

--- src/step14/Bj_10989.java
+++ src/step14/Bj_10989.java
@@ -1,32 +1,28 @@
 package step14;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Bj_10989 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner scan =  new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
 
-		int n = scan.nextInt();
-		int[] arr = new int[n];
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[10001];
 
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = scan.nextInt();	
+		for(int i=0; i<n; i++) {
+			arr[Integer.parseInt(br.readLine())]++;	
 		}
 
-		int temp;
-		for(int i=0; i<arr.length-1; i++) {
-			for(int j =0; j<arr.length-1-i; j++) {
-				if(arr[j] > arr[j+1]) {
-					temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
-		}
 		for(int i=0; i<arr.length; i++)
-			System.out.println(arr[i]);
+			if(arr[i]!=0) {
+				for(int j=0; j<arr[i]; j++)
+					bw.write(i + "\n");
+			}
+		br.close();
+		bw.close();
 	}
 
 }
