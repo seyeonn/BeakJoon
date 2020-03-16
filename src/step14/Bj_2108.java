b5b264e24e614de7080286ad9bdaef35de83fafe
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-03-16 21:31:52 +0900

    0316 해결

--- src/step14/Bj_2108.java
+++ src/step14/Bj_2108.java
@@ -2,6 +2,7 @@ package step14;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Bj_2108 {
@@ -13,51 +14,24 @@ public class Bj_2108 {
 
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-
-		for(int i=0; i<n; i++)
+		int aver = 0;
+		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-
-		bw.write(String.valueOf(aver(n,arr))+"\n");
-		bw.write(String.valueOf(cen(n,arr))+"\n");
-		bw.write(String.valueOf(count(n,arr))+"\n");
-		bw.write(String.valueOf(dis(n,arr))+"\n");
-		br.close();
-		bw.close();
-	}
-	
-	static int aver (int n, int[] arr) {
-		int sum = 0;
-		
-		for(int i=0; i<arr.length; i++)
-			sum += arr[i];
-		
-		return sum/n;
-	}
-	
-	static int cen (int n, int[] arr) {
-		int temp;
-		for(int i=0; i<arr.length-1; i++) {
-			for(int j =0; j<arr.length-1-i; j++) {
-				if(arr[j] > arr[j+1]) {
-					temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
+			aver += arr[i];		
 		}
-		int middle = arr.length/2;
-		
-		return arr[middle];
-	}
 
-	static int count (int n, int[] arr) {
+		Arrays.sort(arr);
+
+		bw.write(Math.round((double)aver/n)+"\n");
+		bw.write(arr[n/2]+"\n");
+
 		int[] num = new int[8001];
 		int temp = 0;
 		ArrayList<Integer> most = new ArrayList<>();
-		
+
 		for(int i=0; i<arr.length; i++) 
 			num[arr[i]+4000]++;
-		
+
 		for(int i=0; i<num.length; i++) {
 			if(temp <= num[i]) {
 				if(temp < num[i]) {
@@ -69,26 +43,19 @@ public class Bj_2108 {
 					most.add(i);
 			}
 		}
-		
+
 		if(most.size() > 1) {
 			Collections.sort(most);
-			return most.get(1) - 4000;
+			bw.write(most.get(1) - 4000 +"\n");
 		}
 		else
-			return most.get(0) - 4000;
+			bw.write(most.get(0) - 4000 +"\n");
+
+		bw.write(arr[arr.length-1] - arr[0] + "\n");
+
+		br.close();
+		bw.close();
 	}
-	
-	static int dis (int n, int[] arr) {
-		int temp;
-		for(int i=0; i<arr.length-1; i++) {
-			for(int j =0; j<arr.length-1-i; j++) {
-				if(arr[j] > arr[j+1]) {
-					temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
-		}
-		return arr[n-1] - arr[0];
-	}
+
 }
+
