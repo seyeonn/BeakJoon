76415e9bae8a68462ee359f1483f15fa992d2cdf
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-03-18 21:28:58 +0900

    0318 해결

--- src/step14/Bj_11650.java
+++ src/step14/Bj_11650.java
@@ -2,6 +2,7 @@ package step14;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Bj_11650 {
 
@@ -14,24 +15,19 @@ public class Bj_11650 {
 		int[][] arr = new int[n][2];
 
 		for(int i=0; i<n; i++) {
-			arr[i][0] = Integer.parseInt(br.readLine());
-			arr[i][1] = Integer.parseInt(br.readLine());
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(str.nextToken());
+			arr[i][1] = Integer.parseInt(str.nextToken());
 		}
 
 		Arrays.sort(arr,(a,b) -> {
+			if(a[0] == b[0]) {
+				return Integer.compare(a[1], b[1]);
+			}
 			return Integer.compare((a[0]), (b[0]));
 		});
 		
-		int temp;
-		for(int i=0; i<arr.length-1; i++) {
-			if(arr[i][0] == arr[i+1][0]) {
-				if(arr[i][1] > arr[i+1][1]) {
-					temp = arr[i][1];
-					arr[i][1] = arr[i+1][1];
-					arr[i+1][1] = temp;
-				}
-			}
-		}
+		
 		for(int i=0; i<arr.length; i++)
 			bw.write(arr[i][0] + " " + arr[i][1] + "\n");
 
