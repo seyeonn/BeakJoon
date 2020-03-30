ba37df8c9e89bf2a3e861ba96270ce2c1ace93a4
seyeonn <ansse17@gmail.com>
2020-03-30 22:20:46 +0900

    0330 해결

--- src/step15/Bj_15651.java
+++ src/step15/Bj_15651.java
@@ -1,26 +1,36 @@
 package step15;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Bj_15651 {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		int m = scan.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
 		int[] arr = new int[m];
 
-
 		dfs(arr,0,n);
+
+		br.close();
+		bw.close();
 	}
 
-	static void dfs(int[] arr, int d, int n) {
+	static void dfs(int[] arr, int d, int n) throws IOException {
 		if(d == arr.length) {
 			for(int i=0; i<arr.length; i++)
-				System.out.print(arr[i] + " ");
-			System.out.println();
+				bw.write(arr[i] + " ");
+			bw.newLine();
 		}
 		else {
 			for(int i=1; i<=n; i++) {
