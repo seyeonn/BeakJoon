13d4ae0fc472f0ea08579caec160094e0733b55f
seyeonn <ansse17@gmail.com>
2020-03-31 18:06:04 +0900

    0331

---
+++ src/etc/Bj_1550.java
@@ -0,0 +1,14 @@
+package etc;
+
+import java.util.Scanner;
+
+public class Bj_1550 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		System.out.println(Integer.parseInt(a,16));
+	}
+
+}
