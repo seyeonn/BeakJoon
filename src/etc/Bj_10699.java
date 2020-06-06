41aa468ce31ee50ef9d06568e55948ce5fb82e7e
seyeonn <ansse17@gmail.com>
2020-06-06 16:03:03 +0900

    0606

---
+++ src/etc/Bj_10699.java
@@ -0,0 +1,15 @@
+package etc;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Bj_10699 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = new Date();
+		System.out.println(format.format(d));
+	}
+
+}
