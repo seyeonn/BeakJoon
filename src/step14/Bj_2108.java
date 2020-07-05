99b7d32ac4944739c25fbdfb58c3f215871a892f
seyeonn <38287375+seyeonn@users.noreply.github.com>
2020-07-05 15:15:50 +0900

    주석 추가

--- src/step14/Bj_2108.java
+++ src/step14/Bj_2108.java
@@ -22,20 +22,20 @@ public class Bj_2108 {
 
 		Arrays.sort(arr);
 
-		bw.write(Math.round((double)aver/n)+"\n");
-		bw.write(arr[n/2]+"\n");
+		bw.write(Math.round((double)aver/n)+"\n");    //산술평균 출력
+		bw.write(arr[n/2]+"\n");                      //중앙값 출력
 
-		int[] num = new int[8001];
-		int temp = 0;
-		ArrayList<Integer> most = new ArrayList<>();
+		int[] num = new int[8001];                    //음수 범위의 배열은 없으므로 -4000 ~ 4000 을 0 ~ 8000 으로 바꿔서 계산
+		int min = 0;
+		ArrayList<Integer> most = new ArrayList<>();  //가장 많이 나타나는 값이 여러 개일 경우
 
 		for(int i=0; i<arr.length; i++) 
 			num[arr[i]+4000]++;
 
 		for(int i=0; i<num.length; i++) {
-			if(temp <= num[i]) {
-				if(temp < num[i]) {
-					temp = num[i];
+			if(min <= num[i]) {
+				if(min < num[i]) {
+					min = num[i];
 					most.clear();
 					most.add(i);
 				}
@@ -44,14 +44,14 @@ public class Bj_2108 {
 			}
 		}
 
-		if(most.size() > 1) {
+		if(most.size() > 1) {                       //최빈값 출력
 			Collections.sort(most);
 			bw.write(most.get(1) - 4000 +"\n");
 		}
 		else
 			bw.write(most.get(0) - 4000 +"\n");
 
-		bw.write(arr[arr.length-1] - arr[0] + "\n");
+		bw.write(arr[arr.length-1] - arr[0] + "\n");  //범위 출력
 
 		br.close();
 		bw.close();
