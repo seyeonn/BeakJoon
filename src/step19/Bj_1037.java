0eaea31655cc16fc6580773fa0d9aedb8f8176bb
Ahn, Seyeon <ansse17@gmail.com>
2020-07-20 17:45:45 +0900

    0720

---
+++ src/step19/Bj_1037.java
@@ -0,0 +1,36 @@
+package step19;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj_1037 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        int result = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        result = arr[0] * arr[n-1];
+
+        bw.write(String.valueOf(result));
+        bw.close();
+        br.close();
+
+	}
+
+}
