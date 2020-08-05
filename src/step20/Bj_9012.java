77f9dd21fcf73c3c6061fbdd70eaa09c81d9e427
Ahn, Seyeon <ansse17@gmail.com>
2020-08-05 15:30:45 +0900

    0805

---
+++ src/step20/Bj_9012.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj_9012 {
+
+	static boolean VPS(String s) {
+		int count = 0;
+		for(char c : s.toCharArray()) {
+			if(c=='(') ++count;
+			else if(c==')') --count;
+			if(count<0)  return false;
+		}
+		return count == 0;
+	}
+
+	public static void main(String[] args) throws NumberFormatException,IOException {
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(reader.readLine());
+		for(int i = 0; i<N; i++) {
+			String s = reader.readLine();
+			System.out.println(VPS(s) ? "YES" :"NO");
+		}
+	}
+
+}
