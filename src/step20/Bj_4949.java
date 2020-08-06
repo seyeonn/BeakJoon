9a9448b525e2b2ebec60c35b0552856f914a7333
Ahn, Seyeon <ansse17@gmail.com>
2020-08-06 16:50:45 +0900

    0806

---
+++ src/step20/Bj_4949.java
@@ -0,0 +1,61 @@
+package step20;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Bj_4949 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] stack = new char[100];
+        int top = -1;
+
+        while(true) {
+        	String str = br.readLine();
+        	if(str.equals("."))
+        		break;
+
+        	loop:
+        	for(int i=0; i<str.length(); i++) {
+        		switch(str.charAt(i)) {
+        		case '[' :
+        			top++;
+        			stack[top] = '[';
+        			break;
+        		case ']' :
+        			if (top == -1 || stack[top] == '(') {
+                        top++;
+                        break loop;
+                    } else
+                        top--;
+                    break;
+        		case '(' :
+        			top++;
+        			stack[top] = '(';
+        			break;
+        		case ')' :
+        			if (top == -1 || stack[top] == '[') {
+                        top++;
+                        break loop;
+                    } else
+                        top--;
+                    break;
+        		}
+        	}
+        	if(top == -1)
+        		bw.write("yes\n");
+        	else
+        		bw.write("no\n");
+
+        	top = -1;
+        }
+        br.close();
+        bw.close();
+	}
+
+}
